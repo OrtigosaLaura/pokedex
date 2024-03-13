c04b5e05050108ce51c84630e043123e70070e88
OrtigosaLaura <lauraortigosa378@gmail.com>
2024-03-13 14:46:16 -0300

    pokedex pag 133

--- Pokedex/Models/Pokemon.cs
+++ Pokedex/Models/Pokemon.cs
@@ -4,7 +4,7 @@ namespace Pokedex.Models;
    {
    public int Numero { get; set; }   
    public string Nome { get; set; }
-   public string Descrição { get; set; }
+   public string Descricao { get; set; }
    public string Especie { get; set; }
    public List<string> Tipo { get; set; } = [];
    public double Altura { get; set; }
